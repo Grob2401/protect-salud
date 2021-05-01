75470642e3169730c8d808b3d36aeb41548d020b
Miguel Yance <miguelygonzales@gmail.com>
2021-04-30 23:19:25 -0500

    PRE-FACTURACION AVANCE 1

---
+++ src/AccesoDatos/ADPrefacturaciones.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using Entidades;
+using System.Configuration;
+
+namespace AccesoDatos
+{
+    public class ADPrefacturaciones
+    {
+        public String dataProviderName = ConfigurationManager.ConnectionStrings["PROVEEDOR_ADONET"].ProviderName;
+        public String connectionString = ConfigurationManager.ConnectionStrings["PROVEEDOR_ADONET"].ConnectionString;
+
+        public List<ENPreFacturacion> ObtenerTodos(string anio,string mes,string desde,string hasta,string option)
+        {
+            DbCommand oCommand = null;
+            List<ENPreFacturacion> oListaPreFacturaciones = new List<ENPreFacturacion>();
+            try
+            {
+                oCommand = GenericDataAccess.CreateCommand(dataProviderName, connectionString, "usp_Facturacion_Calcula");
+                //'2010', '06','20100601', '20100630','0'
+                GenericDataAccess.AgregarParametro(oCommand, "@anoProceso", anio, TipoParametro.STR, Direccion.INPUT);
+                GenericDataAccess.AgregarParametro(oCommand, "@mesProceso", mes, TipoParametro.STR, Direccion.INPUT);
+                GenericDataAccess.AgregarParametro(oCommand, "@fchDesde", desde, TipoParametro.STR, Direccion.INPUT);
+                GenericDataAccess.AgregarParametro(oCommand, "@fchHasta", hasta, TipoParametro.STR, Direccion.INPUT);
+                GenericDataAccess.AgregarParametro(oCommand, "@pcsEspecial", option, TipoParametro.STR, Direccion.INPUT);
+
+                DbDataReader oDataReader = GenericDataAccess.ExecuteReader(oCommand);
+                while (oDataReader.Read())
+                {
+                    ENPreFacturacion oENPreFacturacion = new ENPreFacturacion();
+                    oENPreFacturacion.CodigoCliente = oDataReader["CodigoCliente"].ToString();
+                    oENPreFacturacion.RucDni = oDataReader["RucDni"].ToString();
+                    oENPreFacturacion.RazonSocial = oDataReader["RazonSocial"].ToString();
+                    oENPreFacturacion.DescripcionTipoAsegurado = oDataReader["DescripcionTipoAsegurado"].ToString();                    
+                    oENPreFacturacion.RegCounts = oDataReader["RegCounts"].ToString();
+                    oENPreFacturacion.AporteAfiliados = Convert.ToDouble(oDataReader["AporteAfiliados"]);
+                    oENPreFacturacion.AporteEmpresas = Convert.ToDouble(oDataReader["AporteEmpresas"]);
+                    oListaPreFacturaciones.Add(oENPreFacturacion);
+                }
+                return oListaPreFacturaciones;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception();
+            }
+            finally
+            {
+                GenericDataAccess.CerrarConexion(oCommand, null);
+            }
+        }
+
+        public List<ENPrefacturacionDetalleContratos> Contratos(string anio, string mes, string psespecial, string status, string cliente,string tipoAseg)
+        {
+            DbCommand oCommand = null;
+            List<ENPrefacturacionDetalleContratos> oENPreFacturacionDetalleContratos = new List<ENPrefacturacionDetalleContratos>();
+            try
+            {
+                oCommand = GenericDataAccess.CreateCommand(dataProviderName, connectionString, "usp_Facturacion_Detalle_Contratos");
+                //'2010', '06','20100601', '20100630','0'
+                GenericDataAccess.AgregarParametro(oCommand, "@anoProceso", anio, TipoParametro.STR, Direccion.INPUT);
+                GenericDataAccess.AgregarParametro(oCommand, "@mesProceso", mes, TipoParametro.STR, Direccion.INPUT);
+                GenericDataAccess.AgregarParametro(oCommand, "@pcsEspecial", psespecial, TipoParametro.STR, Direccion.INPUT);
+                GenericDataAccess.AgregarParametro(oCommand, "@pcsStatus", status, TipoParametro.STR, Direccion.INPUT);
+                GenericDataAccess.AgregarParametro(oCommand, "@CodigoCliente", cliente, TipoParametro.STR, Direccion.INPUT);
+                GenericDataAccess.AgregarParametro(oCommand, "@DescripcionTipoAsegurado", tipoAseg, TipoParametro.STR, Direccion.INPUT);
+
+                DbDataReader oDataReader = GenericDataAccess.ExecuteReader(oCommand);
+                while (oDataReader.Read())
+                {
+                    ENPrefacturacionDetalleContratos oENPreFacturacionDetalleContrato = new ENPrefacturacionDetalleContratos();
+                    oENPreFacturacionDetalleContrato.CodigoCliente = oDataReader["CodigoCliente"].ToString();
+                    oENPreFacturacionDetalleContrato.CodigoPlan = oDataReader["CodigoPlan"].ToString();
+                    oENPreFacturacionDetalleContrato.CodigoTitular = oDataReader["CodigoTitular"].ToString();
+                    oENPreFacturacionDetalleContrato.Categoria = oDataReader["Categoria"].ToString();
+                    oENPreFacturacionDetalleContrato.CodigoTipoTrabajador = oDataReader["CodigoTipoTrabajador"].ToString();
+                    oENPreFacturacionDetalleContrato.FchIniAsgContrato = Convert.ToDateTime(oDataReader["FchIniAsgContrato"]);
+                    oENPreFacturacionDetalleContrato.FchNacimiento = Convert.ToDateTime(oDataReader["FchNacimiento"]);
+                    oENPreFacturacionDetalleContrato.Titular = oDataReader["Titular"].ToString();
+                    oENPreFacturacionDetalleContrato.Asegurado = oDataReader["Asegurado"].ToString();
+                    oENPreFacturacionDetalleContrato.DescripcionTipoAsegurado = oDataReader["DescripcionTipoAsegurado"].ToString();
+                    oENPreFacturacionDetalleContrato.CodigoPrima = oDataReader["CodigoPrima"].ToString();
+                    oENPreFacturacionDetalleContrato.DescripcionPrima = oDataReader["DescripcionPrima"].ToString();
+                    oENPreFacturacionDetalleContrato.AporteAfiliado = Convert.ToDouble(oDataReader["AporteAfiliado"]);
+                    oENPreFacturacionDetalleContratos.Add(oENPreFacturacionDetalleContrato);
+                }
+                return oENPreFacturacionDetalleContratos;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception();
+            }
+            finally
+            {
+                GenericDataAccess.CerrarConexion(oCommand, null);
+            }
+        }
+
+    }
+}
---
+++ src/Salud/Controllers/PreFacturacionController.cs
@@ -0,0 +1,97 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LogicaNegocio;
+using Utilitarios;
+using Salud.App_Start;
+using System.Reflection;
+using System.Data;
+using System.IO;
+using ExcelDataReader;
+using System.Runtime.Caching;
+using System.Web.SessionState;
+
+namespace Salud.Controllers
+{
+    public class PreFacturacionController : Controller
+    {
+
+        // GET: SCTRCotizacion
+        private LNUbigeoDpto LNUbigeoDpto = new LNUbigeoDpto();
+        private LNUbigeoProv LNUbigeoProv = new LNUbigeoProv();
+        private LNUbigeoDist LNUbigeoDist = new LNUbigeoDist();
+        private LNSCTRCotizacionesDetalle LNSCTRCotizacionesDetalle = new LNSCTRCotizacionesDetalle();
+        private ENDatosEmitir oEnDatosEmitir = new ENDatosEmitir();
+        public string v_empresaruc = "";
+        public string v_empresanombre = "";
+        public string v_codigocotizacion = "";
+
+        public string v_codigocliente = "";
+        public double v_detmontoplanillaadm = 0;
+        public double v_detmontoplanillaope = 0;
+        public string v_codigocentrocosto = "";
+        public string v_descripcioncentrocosto = "";
+
+        ObjectCache cache = MemoryCache.Default;
+        [SessionExpire]
+        public ActionResult Index()
+        {
+            if (TempData["PREFAC"] != null)
+            {
+                ViewBag.PreFacturaciones = TempData["PREFAC"];
+                ViewBag.y = TempData["Y"];
+                ViewBag.m = TempData["M"];
+            }
+
+            if (TempData["CONTRATOS"] != null)
+            {
+                ViewBag.Contratos = TempData["CONTRATOS"];
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buscar(string anios = " ", string meses = " ", string fechadesde = " ", string fechahasta = " ", string option = " ")
+        {
+            var f1 = "";
+            var f2 = "";
+            var op = "0";
+
+            if (fechadesde != "" && fechahasta != "")
+            {
+                f1 = fechadesde.Replace("-", "");
+                f2 = fechahasta.Replace("-", "");
+                op = "0";
+            }
+
+            TempData["Y"] = anios;
+            TempData["M"] = meses;
+            TempData["PREFAC"] = LNPreFacturaciones.ObtenerTodos(anios, meses, f1, f2, op);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public JsonResult Detalle(string anoProceso = " ",
+                                    string mesProceso = " ",
+                                    string pcsEspecial = " ",
+                                    string pcsStatus = " ",
+                                    string CodigoCliente = " ",
+                                    string DescripcionTipoAsegurado = " ")
+        {
+
+            TempData["CONTRATOS"] = LNPreFacturaciones.Contratos(anoProceso, mesProceso, pcsEspecial, pcsStatus, CodigoCliente, DescripcionTipoAsegurado);
+            return Json(TempData["CONTRATOS"], JsonRequestBehavior.AllowGet);
+        }
+
+
+        //// GET: PreFacturacion
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
